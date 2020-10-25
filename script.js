2c79b97adcbca92b73400656de9cba4e51f2a019
PCallovi <Pcallovi@gmail.com>
2020-10-25 14:27:02 -0400

    finsihed basic solution

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const gameContainer = document.getElementById("game");
 let card1 = null;
 let card2 = null;
-let isClicked = false;
-let complete = 0;
+let cardsFlipped = 0;
+let noClicking = false;
 
 
 const COLORS = [
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple",
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple"
+  "url(https://vignette.wikia.nocookie.net/disney/images/e/e7/Planes_logo.png/revision/latest?cb=20140301215209)",
+  "url(https://vignette.wikia.nocookie.net/disney/images/4/40/Cars-Logo.png/revision/latest?cb=20141115144525)",
+  "url(https://vignette.wikia.nocookie.net/disney/images/f/fa/Mater-ghostlight-disneyscreencaps.com-212.jpg/revision/latest?cb=20160525000145)",
+  "url(https://vignette.wikia.nocookie.net/disney/images/d/d2/Flash-New-Look.png/revision/latest?cb=20171023184830)",
+  "url(https://vignette.wikia.nocookie.net/disney/images/8/83/LightningCarsGame.jpg/revision/latest?cb=20150619221018)",
+  "url(https://vignette.wikia.nocookie.net/disney/images/e/e7/Planes_logo.png/revision/latest?cb=20140301215209)",
+  "url(https://vignette.wikia.nocookie.net/disney/images/4/40/Cars-Logo.png/revision/latest?cb=20141115144525)",
+  "url(https://vignette.wikia.nocookie.net/disney/images/f/fa/Mater-ghostlight-disneyscreencaps.com-212.jpg/revision/latest?cb=20160525000145)",
+  "url(https://vignette.wikia.nocookie.net/disney/images/d/d2/Flash-New-Look.png/revision/latest?cb=20171023184830)",
+  "url(https://vignette.wikia.nocookie.net/disney/images/8/83/LightningCarsGame.jpg/revision/latest?cb=20150619221018)"
 ];
 
 // here is a helper function to shuffle an array
@@ -64,10 +64,46 @@ function createDivsForColors(colorArray) {
 
 // TODO: Implement this function!
 function handleCardClick(event) {
-  // you can use event.target to see which element was clicked
-  console.log("you just clicked", event.target);
-  if (isClicked) return;
-  if (e.target.classList.contains("flipped")) return;
+  if (noClicking) return;
+  if (event.target.classList.contains("flipped")) return;
+
+  let currentCard = event.target;
+  currentCard.style.backgroundImage = currentCard.classList[0];
+
+  if (!card1 || !card2) {
+    currentCard.classList.add("flipped");
+    card1 = card1 || currentCard;
+    card2 = currentCard === card1 ? null : currentCard;
+  }
+
+  if (card1 && card2) {
+    noClicking = true;
+    // debugger
+    let gif1 = card1.className;
+    let gif2 = card2.className;
+
+    if (gif1 === gif2) {
+      cardsFlipped += 2;
+      card1.removeEventListener("click", handleCardClick);
+      card2.removeEventListener("click", handleCardClick);
+      card1 = null;
+      card2 = null;
+      noClicking = false;
+    } else {
+      setTimeout(function() {
+        card1.style.backgroundImage = "";
+        card2.style.backgroundImage = "";
+        card1.classList.remove("flipped");
+        card2.classList.remove("flipped");
+        card1 = null;
+        card2 = null;
+        noClicking = false;
+      }, 1000);
+    }
+  }
+
+  if (cardsFlipped === COLORS.length) alert("game over!");
+
 }
 
 // when the DOM loads
